d5892f3a7580524500bb38b5780f872c21d894f8
Admin <v.i.p@bk.ru>
2024-04-17 20:52:54 +0300

    HW 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+                System.out.println("Hello world!");
+                var dog = 8.0 ;
+                System.out.println(dog);
+                var cat = 3.6 ;
+                System.out.println(cat);
+                var paper = 763789 ;
+                System.out.println(paper);
 
+
+                dog = dog + 4 ;
+                System.out.println("dog = " + dog);
+                cat += 4 ;
+                System.out.println("cat = " + cat);
+                System.out.println("paper = " + (paper + 4));
+                System.out.println("paper = " + paper);
 
 
 
