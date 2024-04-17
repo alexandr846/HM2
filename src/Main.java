4428505df8742b842a85374d8ca63dabbcffdcec
Admin <v.i.p@bk.ru>
2024-04-17 20:39:36 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        var dog = 8.0 ;
-        System.out.println(dog);
-        var cat = 3.6 ;
-        System.out.println(cat);
-        var paper = 763789 ;
-        System.out.println(paper);
 
-
-        dog = dog + 4 ;
-        System.out.println("dog = " + dog);
-        cat += 4 ;
-        System.out.println("cat = " + cat);
-        System.out.println("paper = " + (paper + 4));
-        System.out.println("paper = " + paper);
-
-        var a = 20 ;
-        var b = 7 ;
-        var c = a % b ;
-        System.out.println("c = " + c);
 
 
 
